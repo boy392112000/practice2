b37cdc8ef73a6f600e3093a6963535057842880c
boy392112000 <boy392112000@gmail.com>
2023-06-11 11:25:24 +0800

    循環數字

---
+++ Number Loop Generator/NumberGenerator.cs
@@ -0,0 +1,22 @@
+﻿namespace Number_Loop_Generator
+{
+    internal class NumberGenerator
+    {
+        private int min;
+        private int max;
+        private int current;
+
+        public NumberGenerator(int min, int max)
+        {
+            this.min = min;
+            this.max = max;
+            this.current = min;
+        }
+        public int GetNumber()
+        {
+            int number = current;
+            current = (current + 1) % (max + 1); // 循環取得數字
+            return number;
+        }
+    }
+}
---
+++ Number Loop Generator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Loop_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //撰寫一個可以循環取得 int 的函數
+            var generator = new NumberGenerator(0, 3); // 依序傳回 0-3
+            for (int i = 0; i < 6; i++)
+            {
+                int number = generator.GetNumber(); // 依序傳回0、1、2、3、0、1
+                Console.WriteLine(number+",");
+            }
+        }
+
+        
+    }
+}
